ebdcf01d275b55a1a56a71c581c05bc06e7738dc
Kevin Schans <kevinvdsd@hotmail.com>
2023-05-20 15:59:22 +0200

    Add images

--- src/components/pet-list/pet-list.ts
+++ src/components/pet-list/pet-list.ts
@@ -1,13 +1,53 @@
-import {Component} from "../component";
-import {Pet} from "../../models/pet";
-import {getPets} from "../../data/get-pets";
+import {Component} from '../component';
+import type {Pet} from '../../models/pet';
+import {getPets} from '../../data/get-pets';
+import './pet-list.css';
 
 export class List extends Component {
-  pets: Pet[];
+	pets: Pet[];
+	constructor(selector: string) {
+		super(selector);
+		this.pets = getPets();
+		this.render();
+	}
 
-  constructor(selector: string) {
-    super(selector);
-    this.pets = getPets();
-    this.render();
-  }
-};
+	render() {
+		super.cleanHtml(this.selector);
+		this.template = this.createTemplate();
+		const element = super.render();
+
+		document
+			.querySelectorAll('.button')
+			.forEach(button => {
+				button.addEventListener('click', this.handleDelete.bind(this));
+			},
+			);
+
+		return element;
+	}
+
+	handleDelete(event: Event) {
+		const element = event.target as HTMLParagraphElement;
+		this.pets = this.pets.filter(pet => pet.id !== element.dataset.id);
+		this.render();
+	}
+
+	createTemplate() {
+		const list = this.pets
+			.map(
+				pet => `
+    <li>
+      <img src="./public/assets/${pet.name}.png" >
+      <p>Id: ${pet.id}</p>
+      <p>Name: ${pet.name}</p>
+      <p>Breed: ${pet.breed}</p>
+      <p>Adopted: <input type="checkbox" ${pet.isAdopted ? 'checked' : ''}><p>
+      <p>Owner: ${pet.owner}</p>
+      <p data-id=${pet.id} class="button" role="button">X</p>
+    </li>`,
+			)
+			.join('');
+
+		return `<ul>${list}</ul>`;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@
 import './style.css';
 import {Header} from './components/header/header';
 import {Main} from './components/main/main';
+import {List} from './components/pet-list/pet-list';
+import {Footer} from './components/footer/footer';
 
 new Header('#app');
 new Main('#app');
-
+new List('main');
+new Footer('#app');
